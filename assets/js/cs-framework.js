8e1caf9b9bb4f9db686a7250898a30743c27980a
Codestar <codestarlive@gmail.com>
2017-12-18 21:09:10 +0300

    Fixed gallery field for click edit to empty-show

--- assets/js/cs-framework.js
+++ assets/js/cs-framework.js
@@ -370,93 +370,72 @@
           $list   = $this.find('ul'),
           $input  = $this.find('input'),
           $img    = $this.find('img'),
-          wp_media_frame,
-          wp_media_click;
+          wp_media_frame;
 
       $this.on('click', '.cs-add, .cs-edit', function( e ) {
 
         var $el   = $(this),
+            ids   = $input.val(),
             what  = ( $el.hasClass('cs-edit') ) ? 'edit' : 'add',
-            state = ( what === 'edit' ) ? 'gallery-edit' : 'gallery-library';
+            state = ( what === 'add' && !ids.length ) ? 'gallery' : 'gallery-edit';
 
         e.preventDefault();
 
         // Check if the `wp.media.gallery` API exists.
-        if ( typeof wp === 'undefined' || ! wp.media || ! wp.media.gallery ) {
-          return;
-        }
+        if ( typeof wp === 'undefined' || ! wp.media || ! wp.media.gallery ) { return; }
 
-        // If the media frame already exists, reopen it.
-        if ( wp_media_frame ) {
+         // Open media with state
+        if( state === 'gallery' ) {
+
+          wp_media_frame = wp.media({
+            library: {
+              type: 'image'
+            },
+            frame: 'post',
+            state: 'gallery',
+            multiple: true
+          });
+
           wp_media_frame.open();
-          wp_media_frame.setState(state);
-          return;
+
+        } else {
+
+          wp_media_frame = wp.media.gallery.edit( '[gallery ids="'+ ids +'"]' );
+
+          if( what === 'add' ) {
+            wp_media_frame.setState('gallery-library');
+          }
+
         }
 
-        // Create the media frame.
-        wp_media_frame = wp.media({
-          library: {
-            type: 'image'
-          },
-          frame: 'post',
-          state: 'gallery',
-          multiple: true
-        });
+        // Media Update
+        wp_media_frame.on( 'update', function( selection ) {
 
-        // Open the media frame.
-        wp_media_frame.on('open', function() {
+          $list.empty();
 
-          var ids = $input.val();
+          var selectedIds = selection.models.map( function ( attachment ) {
 
-          if ( ids ) {
+            var item  = attachment.toJSON();
+            var thumb = ( typeof item.sizes.thumbnail !== 'undefined' ) ? item.sizes.thumbnail.url : item.url;
 
-            var get_array = ids.split(',');
-            var library   = wp_media_frame.state('gallery-edit').get('library');
+            $list.append('<li><img src="'+ thumb +'"></li>');
 
-            wp_media_frame.setState(state);
-
-            get_array.forEach(function(id) {
-              var attachment = wp.media.attachment(id);
-              library.add( attachment ? [ attachment ] : [] );
-            });
-
-          }
-        });
-
-        // When an image is selected, run a callback.
-        wp_media_frame.on( 'update', function() {
-
-          var inner  = '';
-          var ids    = [];
-          var images = wp_media_frame.state().get('library');
-
-          images.each(function(attachment) {
-
-            var attributes = attachment.attributes;
-            var thumbnail  = ( typeof attributes.sizes.thumbnail !== 'undefined' ) ? attributes.sizes.thumbnail.url : attributes.url;
-
-            inner += '<li><img src="'+ thumbnail +'"></li>';
-            ids.push(attributes.id);
+            return item.id;
 
           });
 
-          $input.val(ids).trigger('change');
-          $list.html('').append(inner);
+          $input.val( selectedIds.join( ',' ) ).trigger('change');
           $remove.removeClass('hidden');
           $edit.removeClass('hidden');
 
         });
-
-        // Finally, open the modal.
-        wp_media_frame.open();
-        wp_media_click = what;
 
       });
 
       // Remove image
       $remove.on('click', function( e ) {
         e.preventDefault();
-        $list.html('');
+        $list.empty();
         $input.val('').trigger('change');
         $remove.addClass('hidden');
         $edit.addClass('hidden');
